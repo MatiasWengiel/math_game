5437a2a4e022895d59ea30136869b7168823ac0f
Matias Wengiel <matiaswengiel@gmail.com>
2022-08-24 11:08:42 -0500

    Add logic to compare player response to correct sum and update lives accordingly. Add logic to repeat prompt switching the playing user until one of the players' lives == 0 (at which point the game ends)

--- game.rb
+++ game.rb
@@ -8,14 +8,39 @@ class Game
     rand(1..20)
   end
 
-  def prompt
+  def prompt(chosen_player)
+    #Determines the player not currently answering questions
+    other_player = chosen_player == @player1 ? @player2 : @player1
+
+    #Math variables
     num1 = self.random_num
     num2 = self.random_num
     sum = num1 + num2
-
-    pp "#{@player1.name}: What does #{num1} plus #{num2} equal?"
+    
+    #Asks question to chosen player, then awaits a response
+    pp "#{chosen_player.name}: What does #{num1} plus #{num2} equal?"
     response = gets.chomp.to_i
-    pp response
+    
+    #Compares response to correct answer and updates lives accordingly
+    if response == sum
+      pp "#{chosen_player.name}: YES! You are correct."
+      pp "#{chosen_player.name}: #{chosen_player.lives}/3 vs #{other_player.name}: #{other_player.lives}/3"
+    else
+      pp "#{chosen_player.name}: Seriously? No!"
+      chosen_player.lives-= 1
+      pp "#{chosen_player.name}: #{chosen_player.lives}/3 vs #{other_player.name}: #{other_player.lives}/3"
+    end
+
+    #Ends game if chosen_player is out of lives at the end of the last prompt. Otherwise, runs prompt using other_player as the chosen one
+    if chosen_player.lives == 0
+      pp "----- WE HAVE A WINNER!! -----"
+      pp "#{other_player.name} wins with a score of #{other_player.lives}/3"
+      pp '----- GAME OVER -----'
+      pp 'Good bye!'
+    else
+     pp '----- NEW TURN -----'
+     prompt(other_player)
+    end
     
   end
 
